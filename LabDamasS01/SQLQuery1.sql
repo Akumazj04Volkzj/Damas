404e1d8d00a85db8132d1cbd8c1635d85ddf524b
Bruno Bras <diogo_vsc_99@hotmail.com>
2019-05-16 15:16:31 +0100

    sql

---
+++ LabDamasS01/SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿
+CREATE TABLE Jogador(
+	Id			INT				IDENTITY(1,1)		NOT NULL,
+	Nome		VARCHAR(25)							NOT NULL,
+	Username	VARCHAR(10)							NOT NULL,
+	Email		VARCHAR(40)							NOT NULL,
+	Nacionalidade	VARCHAR(30)						NOT NULL,
+	)
